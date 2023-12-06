917416ef355d7929c13ae0b8e144d5f0cccfec09
Lenny7120 <ocrank20@gmail.com>
2023-12-06 09:30:59 -0800

    Airbnb task

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,18 @@ class BaseModel:
         Defines the common attributes & methods.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialized a new BaseModel
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'update_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                else:
+                    self.id = str(uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = datetime.now()
 
     def __str__(self):
         """ Returns a printable representation of the obj.
