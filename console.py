157b91e0fc384f9739b01ef83ec94b4d1ef8eb47
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-12-08 14:06:03 +0000

    fix: circular import

--- console.py
+++ console.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ a console for the Airbnb project using cmd module"""
 import cmd
-from models.amenity import Amenity
+# from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+# from models.city import City
+# from models.place import Place
+# from models.review import Review
+# from models.state import State
+# from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
